05d8716be2f33af91c64965c4c8899dcacebf11a
Federico Matteoni <fedex15@live.it>
2019-11-19 21:56:59 +0100

    Arguments and error parsing

--- src/com/fexed/lprb/UDPPing/UDPPingServer.java
+++ src/com/fexed/lprb/UDPPing/UDPPingServer.java
@@ -1,12 +1,34 @@
 package com.fexed.lprb.UDPPing;
 
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+/**
+ * @author Federico Matteoni
+ */
 public class UDPPingServer implements Runnable{
-    public static void main(String[] args) throws InterruptedException {
-        new Thread(new UDPPingServer()).start();
+    private int port;
+
+    public UDPPingServer(int port) {
+        this.port = port;
     }
 
     @Override
     public void run() {
+        try {
+            DatagramSocket dtgSkt = new DatagramSocket(this.port);
+        } catch (SocketException ignored) {}
+    }
 
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length != 1) System.err.println("Usage: java UDPPingServer port");
+        else {
+            try {
+                int port = Integer.parseInt(args[0]);
+                new Thread(new UDPPingServer(port)).start();
+            }
+            catch (NumberFormatException ex) { System.err.println("ERR - arg 1"); }
+
+        }
     }
 }
