9967bcb1d65305be1e4e5ab1a6f8eef4627349da
Federico Matteoni <fedex15@live.it>
2019-11-19 21:59:29 +0100

    Arguments and error parsing

--- src/com/fexed/lprb/UDPPing/UDPPingClient.java
+++ src/com/fexed/lprb/UDPPing/UDPPingClient.java
@@ -1,12 +1,29 @@
 package com.fexed.lprb.UDPPing;
 
+/**
+ * @author Federico Matteoni
+ */
 public class UDPPingClient implements Runnable {
-    public static void main(String[] args) throws InterruptedException {
-        new Thread(new UDPPingClient()).start();
+    private String hostname;
+    private int port;
+
+    public UDPPingClient(String hostname, int port) {
+        this.hostname = hostname;
+        this.port = port;
     }
 
     @Override
     public void run() {
 
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length != 2) System.err.println("Usage: java UDPPingClient hostname port");
+        else {
+            try {
+                int port = Integer.parseInt(args[1]);
+                new Thread(new UDPPingClient(args[0], port)).start();
+            } catch (NumberFormatException ex) { System.err.println("ERR - arg 2"); }
+        }
     }
 }
