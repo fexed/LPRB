bbf8e2271c0cd9b83c09a7c017ab279c2c9b799d
Federico Matteoni <fedex15@live.it>
2019-11-20 12:51:50 +0100

    UDP Ping MainClass For testing

--- src/com/fexed/lprb/UDPPing/MainClass.java
+++ src/com/fexed/lprb/UDPPing/MainClass.java
@@ -1,8 +1,16 @@
 package com.fexed.lprb.UDPPing;
 
 public class MainClass {
-
     public static void main(String[] args) throws InterruptedException {
+        Thread server = new Thread(new UDPPingServer(1337));
+        Thread client = new Thread(new UDPPingClient("localhost", 1337));
+
+        server.start();
+        Thread.sleep(500);
+        client.start();
+        client.join();
+        System.out.println("Client finished, wait 1m for server timeout");
+        server.join();
 
     }
 }
